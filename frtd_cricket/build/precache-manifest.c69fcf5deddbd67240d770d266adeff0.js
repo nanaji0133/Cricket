5ebbdc3a2b20f3ac03328912bf63f65193ee1dbd
nanaji0133 <nanajisanka5357@gmail.com>
2020-06-12 16:47:42 -0400

    glkaag

---
+++ frtd_cricket/build/precache-manifest.c69fcf5deddbd67240d770d266adeff0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e36d37472c613f4a9fd386adf833b4e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "676acf316fb9035547d8",
+    "url": "/static/css/main.97b5d71e.chunk.css"
+  },
+  {
+    "revision": "374c0ca4f3245e7e6431",
+    "url": "/static/js/2.5f72ad4d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5f72ad4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "676acf316fb9035547d8",
+    "url": "/static/js/main.86580f0c.chunk.js"
+  },
+  {
+    "revision": "b37848427de1cb3d0204",
+    "url": "/static/js/runtime-main.d0e41bdf.js"
+  }
+]);
---
+++ frtd_cricket/src/components/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import "../assests/style.css";
+// import "../index.css";
+import Footer from "./Footer";
+import Header from "./Header";
+import Team from "./Team";
+
+
+class App extends Component
+{
+    render ()
+    {
+        return (
+            <div className="container">
+                <Header />
+                <Team />
+                <Footer />
+            </div>
+        );
+    }
+}
+
+export default App;
